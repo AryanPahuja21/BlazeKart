8a38744f94e66b31b4180f0c3f6599cce9009ffb
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-04-23 03:05:39 +0530

    express initialized

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,14 @@
 import "dotenv/config";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  });
