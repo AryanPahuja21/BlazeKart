cc69978de6d9ef460779fdc8897bc564b7037820
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-04-24 21:08:25 +0530

    added asyncHandler utility

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,12 +8,16 @@ app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-  }),
+  })
 );
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
 
 export { app };
