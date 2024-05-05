e2296a761efd51d6150f9e6bbb97742632d656c5
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-05-05 13:37:41 +0530

    frontend started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy } from "react";
+
+const Home = lazy(() => import("./pages/Home/Home"));
+const About = lazy(() => import("./pages/Explore/Explore"));
+const Contact = lazy(() => import("./pages/Cart/Cart"));
+const NotFound = lazy(() => import("./pages/NotFound/NotFound"));
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<About />} />
+        <Route path="/cart" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
