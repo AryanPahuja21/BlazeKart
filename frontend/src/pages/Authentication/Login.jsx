36ec3b0a84ac1d179eb17f054c0c949f44867b25
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-05-22 01:07:11 +0530

    auth pages protected

--- frontend/src/pages/Authentication/Login.jsx
+++ frontend/src/pages/Authentication/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import Navbar from "../../components/Navbar/Navbar";
 import { loginUser } from "../../redux/reducer/userReducer";
@@ -14,6 +15,7 @@ const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { loading, error } = useSelector((state) => state.user);
+  let user = localStorage.getItem("user");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -35,72 +37,78 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <Navbar />
-      <div className="flex justify-center mt-48">
-        <div className="">
-          <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-96">
-            <h2 className="text-2xl font-bold mb-6">User Login</h2>
-            {emptyFieldWarning && (
-              <p className="text-red-500 mb-4">Please fill in all fields</p>
-            )}
-            {error && <p className="text-red-500 mb-4">{error}</p>}
-            <form action="POST">
-              <div className="mb-4">
-                <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="email"
-                >
-                  Email
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-2 focus:border-orange-500 focus:ring-0"
-                  id="email"
-                  type="email"
-                  placeholder="Enter your email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
+    <>
+      {user ? (
+        <Navigate to="/" />
+      ) : (
+        <div>
+          <Navbar />
+          <div className="flex justify-center mt-48">
+            <div className="">
+              <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-96">
+                <h2 className="text-2xl font-bold mb-6">User Login</h2>
+                {emptyFieldWarning && (
+                  <p className="text-red-500 mb-4">Please fill in all fields</p>
+                )}
+                {error && <p className="text-red-500 mb-4">{error}</p>}
+                <form action="POST">
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="email"
+                    >
+                      Email
+                    </label>
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-2 focus:border-orange-500 focus:ring-0"
+                      id="email"
+                      type="email"
+                      placeholder="Enter your email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="password"
+                    >
+                      Password
+                    </label>
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-2 focus:border-orange-500 focus:ring-0"
+                      id="password"
+                      type="password"
+                      placeholder="Enter your password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                  <div className="flex items-center justify-between mt-5">
+                    <button
+                      className="bg-orange-700 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                      type="submit"
+                      onClick={handleSubmit}
+                    >
+                      {loading ? "Loading..." : "Login"}
+                    </button>
+                  </div>
+                  <div className="mt-2">
+                    <span>Already have an account? </span>
+                    <span
+                      className="text-blue-500 hover:text-blue-700 cursor-pointer font-bold"
+                      onClick={() => navigate("/signup")}
+                    >
+                      Register
+                    </span>
+                  </div>
+                </form>
               </div>
-              <div className="mb-4">
-                <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="password"
-                >
-                  Password
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-2 focus:border-orange-500 focus:ring-0"
-                  id="password"
-                  type="password"
-                  placeholder="Enter your password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <div className="flex items-center justify-between mt-5">
-                <button
-                  className="bg-orange-700 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  type="submit"
-                  onClick={handleSubmit}
-                >
-                  {loading ? "Loading..." : "Login"}
-                </button>
-              </div>
-              <div className="mt-2">
-                <span>Already have an account? </span>
-                <span
-                  className="text-blue-500 hover:text-blue-700 cursor-pointer font-bold"
-                  onClick={() => navigate("/signup")}
-                >
-                  Register
-                </span>
-              </div>
-            </form>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
--- frontend/src/pages/Authentication/Signup.jsx
+++ frontend/src/pages/Authentication/Signup.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
+import { useSelector } from "react-redux";
 
 const Signup = () => {
   const navigate = useNavigate();
@@ -15,6 +16,8 @@ const Signup = () => {
     gender: "",
     password: "",
   });
+  const { loading, error } = useSelector((state) => state.user);
+  let user = localStorage.getItem("user");
 
   const handleChange = (e) => {
     if (e.target.name === "avatar") {
@@ -59,150 +62,173 @@ const Signup = () => {
 
   return (
     <>
-      <Navbar />
-      <div className="w-[80vw] mx-auto mt-8 bg-white p-8 shadow-lg">
-        <h2 className="text-2xl font-bold mb-4">User Registration</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
-            <section className="mb-4">
-              <div className="mb-4">
-                <label htmlFor="username" className="block font-semibold mb-1">
-                  Username
-                </label>
-                <input
-                  type="text"
-                  id="username"
-                  name="username"
-                  value={formData.username}
-                  onChange={handleChange}
-                  placeholder="Enter a unique username"
-                  className="w-full px-3 py-2 border rounded-md focus:outline-none focus:shadow-outline focus:border-2 focus:border-orange-500 focus:ring-0"
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="email" className="block font-semibold mb-1 ">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  placeholder="Enter your email"
-                  className="w-full px-3 py-2 border rounded-md focus:outline-none focus:shadow-outline focus:border-2 focus:border-orange-500 focus:ring-0"
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="fullname" className="block font-semibold mb-1">
-                  Full Name
-                </label>
-                <input
-                  type="text"
-                  id="fullname"
-                  name="fullname"
-                  value={formData.fullname}
-                  onChange={handleChange}
-                  placeholder="Enter your full name"
-                  className="w-full px-3 py-2 border rounded-md focus:outline-none focus:shadow-outline focus:border-2 focus:border-orange-500 focus:ring-0"
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="dob" className="block font-semibold mb-1">
-                  Date of Birth
-                </label>
-                <input
-                  type="date"
-                  id="dob"
-                  name="dob"
-                  value={formData.dob}
-                  onChange={handleChange}
-                  placeholder="Enter your date of birth"
-                  className="w-full px-3 py-2 border rounded-md focus:outline-none focus:shadow-outline focus:border-2 focus:border-orange-500 focus:ring-0"
-                  required
-                />
-              </div>
-            </section>
-            <section className="mb-4">
-              <div className="mb-4">
-                <label
-                  htmlFor="gender"
-                  className="block font-semibold mb-1 focus:ring-0"
-                >
-                  Gender
-                </label>
-                <select
-                  type="radio"
-                  id="gender"
-                  name="gender"
-                  value={formData.gender}
-                  onChange={handleChange}
-                  className="w-full px-3 py-2 border rounded-md h-10 focus:outline-none focus:shadow-outline focus:border-2 focus:border-orange-500 focus:ring-0 text-black"
-                  required
-                >
-                  <option value="">Select Gender</option>
-                  <option value="male">Male</option>
-                  <option value="female">Female</option>
-                  <option value="other">Other</option>
-                  <option value="prefer-not-to-say">Prefer not to say</option>
-                </select>
-              </div>
-              <div className="mb-4">
-                <label htmlFor="avatar" className="block font-semibold mb-1">
-                  Avatar(will be displayed on user profile)
-                </label>
+      {user ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <Navbar />
+          <div className="w-[80vw] mx-auto mt-8 bg-white p-8 shadow-lg">
+            <h2 className="text-2xl font-bold mb-4">User Registration</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
+                <section className="mb-4">
+                  <div className="mb-4">
+                    <label
+                      htmlFor="username"
+                      className="block font-semibold mb-1"
+                    >
+                      Username
+                    </label>
+                    <input
+                      type="text"
+                      id="username"
+                      name="username"
+                      value={formData.username}
+                      onChange={handleChange}
+                      placeholder="Enter a unique username"
+                      className="w-full px-3 py-2 border rounded-md focus:outline-none focus:shadow-outline focus:border-2 focus:border-orange-500 focus:ring-0"
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="email"
+                      className="block font-semibold mb-1 "
+                    >
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleChange}
+                      placeholder="Enter your email"
+                      className="w-full px-3 py-2 border rounded-md focus:outline-none focus:shadow-outline focus:border-2 focus:border-orange-500 focus:ring-0"
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="fullname"
+                      className="block font-semibold mb-1"
+                    >
+                      Full Name
+                    </label>
+                    <input
+                      type="text"
+                      id="fullname"
+                      name="fullname"
+                      value={formData.fullname}
+                      onChange={handleChange}
+                      placeholder="Enter your full name"
+                      className="w-full px-3 py-2 border rounded-md focus:outline-none focus:shadow-outline focus:border-2 focus:border-orange-500 focus:ring-0"
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="dob" className="block font-semibold mb-1">
+                      Date of Birth
+                    </label>
+                    <input
+                      type="date"
+                      id="dob"
+                      name="dob"
+                      value={formData.dob}
+                      onChange={handleChange}
+                      placeholder="Enter your date of birth"
+                      className="w-full px-3 py-2 border rounded-md focus:outline-none focus:shadow-outline focus:border-2 focus:border-orange-500 focus:ring-0"
+                      required
+                    />
+                  </div>
+                </section>
+                <section className="mb-4">
+                  <div className="mb-4">
+                    <label
+                      htmlFor="gender"
+                      className="block font-semibold mb-1 focus:ring-0"
+                    >
+                      Gender
+                    </label>
+                    <select
+                      type="radio"
+                      id="gender"
+                      name="gender"
+                      value={formData.gender}
+                      onChange={handleChange}
+                      className="w-full px-3 py-2 border rounded-md h-10 focus:outline-none focus:shadow-outline focus:border-2 focus:border-orange-500 focus:ring-0 text-black"
+                      required
+                    >
+                      <option value="">Select Gender</option>
+                      <option value="male">Male</option>
+                      <option value="female">Female</option>
+                      <option value="other">Other</option>
+                      <option value="prefer-not-to-say">
+                        Prefer not to say
+                      </option>
+                    </select>
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="avatar"
+                      className="block font-semibold mb-1"
+                    >
+                      Avatar(will be displayed on user profile)
+                    </label>
 
-                <input
-                  type="file"
-                  id="avatar"
-                  name="avatar"
-                  onChange={handleChange}
-                  className="w-full px-3 py-2 border rounded-md"
-                  accept="image/*"
-                  required
-                  multiple={false}
-                />
+                    <input
+                      type="file"
+                      id="avatar"
+                      name="avatar"
+                      onChange={handleChange}
+                      className="w-full px-3 py-2 border rounded-md"
+                      accept="image/*"
+                      required
+                      multiple={false}
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="password"
+                      className="block font-semibold mb-1"
+                    >
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      value={formData.password}
+                      onChange={handleChange}
+                      placeholder="Enter your password"
+                      className="w-full px-3 py-2 border rounded-md focus:outline-none focus:shadow-outline focus:border-2 focus:border-orange-500 focus:ring-0"
+                      required
+                    />
+                  </div>
+                </section>
               </div>
-              <div className="mb-4">
-                <label htmlFor="password" className="block font-semibold mb-1">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  value={formData.password}
-                  onChange={handleChange}
-                  placeholder="Enter your password"
-                  className="w-full px-3 py-2 border rounded-md focus:outline-none focus:shadow-outline focus:border-2 focus:border-orange-500 focus:ring-0"
-                  required
-                />
+              <div className="flex items-center justify-between mt-5">
+                <button
+                  className="bg-orange-700 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                  onClick={handleSubmit}
+                >
+                  {loading ? "Loading..." : "Register"}
+                </button>
               </div>
-            </section>
+              <div className="mt-2">
+                <span>Don't have an account? </span>
+                <span
+                  className="text-blue-500 hover:text-blue-700 cursor-pointer font-bold"
+                  onClick={() => navigate("/login")}
+                >
+                  Login
+                </span>
+              </div>
+            </form>
           </div>
-          <div className="flex items-center justify-between mt-5">
-            <button
-              className="bg-orange-700 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
-              onClick={handleSubmit}
-            >
-              Signup
-            </button>
-          </div>
-          <div className="mt-2">
-            <span>Don't have an account? </span>
-            <span
-              className="text-blue-500 hover:text-blue-700 cursor-pointer font-bold"
-              onClick={() => navigate("/login")}
-            >
-              Login
-            </span>
-          </div>
-        </form>
-      </div>
+        </>
+      )}
     </>
   );
 };
